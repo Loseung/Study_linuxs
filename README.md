383e62d10d36750ec7ddaee6a6aed4e986e99538
Loseung <loseung00@gmail.com>
2025-07-15 14:32:21 +0900

    세번째 입력

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Study_linuxs
 ## powershell (GUI & CLI)
 연습 글을 올립니다.
-* 첫번째
-* 두번째
+* Searching Command:[명령어](codes/10_powershells.sh)
+* file & directory Command[명령어](codes/10_powershells.sh)
 
-[안내글](codes/10_powershells.sh)
---
+++ codes/powershell.sh
@@ -0,0 +1,27 @@
+PS C:\Develops> mkdir dir_first
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-15   오후 2:31                dir_first
+
+
+PS C:\Develops> ls
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-15   오후 2:31                dir_first
+d-----      2025-07-14  오전 11:58                Downloads
+d-----      2025-07-15   오후 2:22                temp_dir
+d-----      2025-07-15   오후 2:00                temp_dir_graphic
+d-----      2025-07-15   오전 9:34                VMs
+
+
+PS C:\Develops>
