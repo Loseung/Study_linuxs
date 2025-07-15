3940c854ec06463b62b7259d33245871acf035c9
Loseung <loseung00@gmail.com>
2025-07-15 12:23:13 +0900

    두번째 올리기

---
+++ codes/codes/codes/10_powershells.sh
@@ -0,0 +1,24 @@
+PS C:\Users\Administrator> pwd
+
+Path
+----
+C:\Users\Administrator
+
+#현재 경로 확인
+PS C:\Develops> pwd
+
+Path
+----
+C:\Develops
+
+
+PS C:\Develops> ls
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-14  오전 11:58                Downloads
+d-----      2025-07-15   오전 9:34                VMs
